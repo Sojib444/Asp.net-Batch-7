5d3cd7fc1477bc4751d776effa422435032e8c62
Sojib <mdsojibhosen444@gmail.com>
2022-06-23 18:50:40 +0600

    commit

--- src/Assignment-1/Assignment-1/Program.cs
+++ src/Assignment-1/Assignment-1/Program.cs
@@ -38,88 +38,106 @@ void Json_Convertor(Type T)
     }
     Console.WriteLine();
 
-    foreach (PropertyInfo p in proerties)
+    //foreach (PropertyInfo p in proerties)
+    for(int i=0;i<proerties.Length;i++)
     {
-        if (p.PropertyType.IsPrimitive)
+        if (proerties[i].PropertyType.IsPrimitive)
         {
-            Console.Write($"The Property-Name is '{p.Name}' and return types " +
-                $" '{p.PropertyType}' plese set a '{p.PropertyType.Name}' value = ");
-            object? value = Console.ReadLine();
-            object instance = Activator.CreateInstance(T);
-            p.SetValue(instance, Convert.ChangeType(value, p.PropertyType));
-            //Json
-            Json += $" \"{p.Name}\" : {p.GetValue(instance)}";
-            Json += ", \n";
-            //Console.WriteLine(p.GetValue(instatance));
-            //Console.WriteLine();
-        }
-
-        else if (p.PropertyType == typeof(string))
-        {
-
-            Console.Write($"The '{p.Name}' of the {T.Name} = ");
-            object? value = Console.ReadLine();
+            Console.Write($"The '{proerties[i].Name}' of the {T.Name} = ");
+            string? value = Console.ReadLine();
             object? instance = Activator.CreateInstance(T);
-            p.SetValue(instance, Convert.ChangeType(value, p.PropertyType));
-            //Json
-            Json += $" \"{p.Name}\" : \"{p.GetValue(instance)}\"";
-            Json += ", \n";
-            //Console.WriteLine(p.GetValue(instance));
-            //Console.WriteLine();
-
-        }
-        else if (p.PropertyType.IsGenericType)
-        {
-            Type[] generics_paramerters = p.PropertyType.GetGenericArguments();
-            foreach (Type type in generics_paramerters)
+            proerties[i].SetValue(instance, Convert.ChangeType(value, proerties[i].PropertyType));
+            Json += $"\"{proerties[i].Name}\" : \"{value}\"";
+            if (i != proerties.Length - 1)
             {
-                //Json
-                Json += $" \"{p.Name}\" : [";
-                Console.WriteLine($"Now You have to work with '{p.Name}' and return type of " +
-                $"this properties 'List<{type.Name}>' \n");
-                Console.WriteLine($"Plese press 'Enter' and Ready to  Move '{type.Name}'" +
-                                $" class");
-                Console.ReadKey();
-
-                //Json
-                Json += "{";
-                Json_Convertor(type);
-                Json += "},";
-
-
+                Json += ",\n";
             }
-            Json += "],\n";
+
 
 
         }
-        else if (p.PropertyType == typeof(DateTime))
+        else if (proerties[i].PropertyType == typeof(string))
         {
-            Console.Write($"The '{p.Name}' of the {T.Name} in this Formate('month-date-year') = ");
+            Console.Write($"The '{proerties[i].Name}' of the {T.Name} = ");
+            string? value = Console.ReadLine();
+            object? instance = Activator.CreateInstance(T);
+            proerties[i].SetValue(instance, Convert.ChangeType(value, proerties[i].PropertyType));
+            Json += $"\"{proerties[i].Name}\" : \"{value}\"";
+            if (i != proerties.Length - 1)
+            {
+                Json += ",\n";
+            }
+        }
+        else if (proerties[i].PropertyType.IsGenericType)
+        {
+            Type[] generics_paramerters = proerties[i].PropertyType.GetGenericArguments();
+            for (int j = 0; j < generics_paramerters.Length; j++)
+            {
+                Json += $" \"{proerties[i].Name}\" : [";
+                Console.WriteLine($"Now You have to work with '{proerties[i].Name}' and return type of " +
+                                $"this properties 'List<{generics_paramerters[j].Name}>' \n");
+                Console.WriteLine($"Plese press 'Enter' and Ready to  Move '{generics_paramerters[j].Name}'" +
+                $" class");
+                Console.ReadKey();
+                Console.Write($"\nHow Many {generics_paramerters[j].Name} for {generics_paramerters[j].Name}  Do You want to Add? = ");
+                int w = Convert.ToInt32(Console.ReadLine());
+                for (int k = 1; k <= w; k++)
+                {
+                    Json += "{";
+                    Console.WriteLine($"\n--------For {generics_paramerters[j].Name}-> {k}--------");
+                    Json_Convertor(generics_paramerters[j]);
+                    if (k != w)
+                    {
+                        Json += "},";
+                    }
+                    else
+                    {
+                        Json += "}";
+                    }
+                }
+
+                if (i != proerties.Length - 1)
+                {
+                    Json += "],";
+                }
+                else
+                {
+                    Json += "]";
+                }
+            }
+        }
+        else if (proerties[i].PropertyType == typeof(DateTime))
+        {
+            Console.Write($"The '{proerties[i].Name}' of the {T.Name} in this Formate('month-date-year') = ");
             object? value = Console.ReadLine();
             object? instance = Activator.CreateInstance(T);
-            p.SetValue(instance, Convert.ChangeType(value, p.PropertyType));
+            proerties[i].SetValue(instance, Convert.ChangeType(value, proerties[i].PropertyType));
             //Json
-            Json += $" \"{p.Name}\": \"{p.GetValue(instance)}\"";
-            Json += ", \n";
+            Json += $"\"{proerties[i].Name}\" : \"{proerties[i].GetValue(instance)}\"";
+            if (i != proerties.Length - 1)
+            {
+                Json += ",\n";
+            }
             //Console.WriteLine(p.GetValue(instance));
             //Console.WriteLine();
 
         }
-        else if (p.PropertyType.IsClass)
-        {
-            Console.WriteLine($"Now You have to work with '{p.Name}' and return type of " +
-                $"this properties '{p.PropertyType.Name}' \n");
-            Console.WriteLine($"Plese press 'Enter' and Ready to  Move '{p.PropertyType.Name}'" +
+        else if(proerties[i].PropertyType.IsClass)
+          {
+            Console.WriteLine($"\nNow You have to work with '{proerties[i].Name}'of  " +
+                $"and return type of " +$"this properties '{proerties[i].PropertyType.Name}'Class ");
+            Console.WriteLine($"Plese press 'Enter' and Ready to  Move '{proerties[i].PropertyType.Name}'" +
                             $" class");
             Console.ReadKey();
-            Json += $"\"{p.Name}\" : [";
-            Json += "{";
-            Json_Convertor(p.PropertyType);
-            Json += "} \n],";
-
+            Json += $"\"{proerties[i].Name}\" : " + "{";
+            Json_Convertor(proerties[i].PropertyType);
+            if (i != proerties.Length - 1)
+            {
+                Json += "},\n";
+            }
+            else
+                Json += "}\n";
         }
-        
-      
 
 
     }
