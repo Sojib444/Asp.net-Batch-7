ff6cdf9c2949f7054c2ec3c02a3cfc74c404c80b
Sojib <mdsojibhosen444@gmail.com>
2022-07-29 10:13:02 +0600

    commit

---
+++ src/Prolem Solving/66. Plus One--LeetCode/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+
+int[] digits = {2,3,9};
+
+
+
+int carry = 1;
+int[] result = new int[digits.Length];
+for (int incr = digits.Length - 1; incr >= 0; incr--)
+{
+    int total = digits[incr] + carry;
+
+    carry = total == 10 ? 1 : 0;
+
+    result[incr] = total % 10;
+
+}
+
+if (carry == 1)
+{
+    result = new int[digits.Length + 1];
+    result[0] = 1;
+}
+
+foreach(var item in result)
+{
+    Console.WriteLine(item);
+}
---
+++ src/Prolem Solving/Bainary Search/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+
+int[] arr = { 1, 2, 4, 5, 6, 7, 8, 9 };
+int target = 2;
+int L = 0;
+int R = arr.Length - 1;
+int index=-1;
+
+while (L <= R)
+{
+    int mid = L + (R - L) / 2;
+    if (arr[mid] == target)
+    {
+        index = mid;
+    }
+    if (arr[mid] > target)
+    {
+        R = mid - 1;
+    }
+    else
+        L = mid+1;
+
+
+}
+Console.WriteLine(index);
+
+
