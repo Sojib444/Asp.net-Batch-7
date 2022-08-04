3105b55ee20b182182b04e8c9c2f2c7e4a18401a
Sojib <mdsojibhosen444@gmail.com>
2022-08-04 22:35:56 +0600

    commit

---
+++ src/Prolem Solving/442. Find All Duplicates in an Array/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+int[] arr = { 4, 3, 2, 7, 8, 2, 3, 1 };
+
+int[] brr = new int[arr.Length+1];
+
+List<int> list = new List<int>();
+
+for(int i = 0; i <arr.Length; i++)
+{
+    brr[arr[i]]++;
+}
+
+
+for(int i=1;i<brr.Length;i++)
+{
+    if (brr[i] > 1)
+        list.Add(i);
+
+}
+
+
+foreach(var item in list)
+{
+    Console.WriteLine(item);
+}
