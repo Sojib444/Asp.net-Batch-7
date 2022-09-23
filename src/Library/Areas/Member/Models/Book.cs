ac87c2de77f9b3dfa44557d368c19f619cbd848e
Sojib <mdsojibhosen444@gmail.com>
2022-09-23 22:42:22 +0600

    commit

---
+++ src/Library/Areas/Member/Models/Book.cs
@@ -0,0 +1,44 @@
+﻿using Autofac;
+using Infrastructure.BusinessObject;
+using Infrastructure.Service;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library.Areas.Member.Models
+{
+    public class Book
+    {
+        private IBookService _bookservice;
+        private ILifetimeScope _scope;
+        public Book()
+        {
+        }
+
+        public Book(IBookService studentService)
+        {
+            _bookservice = studentService;
+        }
+
+        internal void ResolveDependency(ILifetimeScope scope)
+        {
+            _scope = scope;
+            _bookservice = _scope.Resolve<IBookService>();
+        }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Writer { get; set; }
+        [Required]
+        public int Price { get; set; }
+
+        public async Task CreateStudent()
+        {
+            BBook book = new BBook();
+            book.Name = Name;
+            book.Writer = Writer;
+            book.Price = Price;
+
+            _bookservice.CreateBook(book);
+        }
+    }
+}
---
+++ src/Library/Areas/Member/Models/Reader.cs
@@ -0,0 +1,40 @@
+﻿using Autofac;
+using Infrastructure.BusinessObject;
+using Infrastructure.Service;
+using Microsoft.Build.Framework;
+
+namespace Library.Areas.Member.Models
+{
+    public class Reader
+    {
+        private IReaderService _studentService;
+        private ILifetimeScope _scope;
+        public Reader()
+        {
+        }
+
+        public Reader(IReaderService studentService)
+        {
+            _studentService = studentService;
+        }
+
+        internal void ResolveDependency(ILifetimeScope scope)
+        {
+            _scope = scope;
+            _studentService = _scope.Resolve<IReaderService>();
+        }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Address { get; set; }
+        public async Task CreateReader()
+        {
+            BReader reader = new BReader();
+            reader.Name = Name;
+            reader.Address = Address;
+
+            _studentService.CreateReader(reader);
+        }
+    }
+}
