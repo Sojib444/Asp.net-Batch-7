df0d58340b04096d67f4febb758545eacabad1a6
Sojib <mdsojibhosen444@gmail.com>
2022-09-30 12:56:11 +0600

    cm

---
+++ src/Prolem Solving/Pattern-2/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j <= i; j++)
+    {
+        Console.Write(i+1 + " ");
+    }
+    Console.WriteLine();
+}
+
---
+++ src/Prolem Solving/Pattern-3/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+int d = 0;
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j <= i; j++)
+    {
+        Console.Write(++d + " ");
+    }
+    Console.WriteLine();
+}
