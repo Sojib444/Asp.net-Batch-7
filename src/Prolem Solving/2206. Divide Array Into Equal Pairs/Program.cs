db915c6fc6be1f77881b04c11c73a4de53ddeef1
Sojib <mdsojibhosen444@gmail.com>
2022-10-11 14:07:35 +0600

    commit

---
+++ src/Prolem Solving/2206. Divide Array Into Equal Pairs/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+string[] s = Console.ReadLine().Split(',');
+
+int[] arr = new int[s.Length];
+
+
+for(int i=0;i<s.Length;i++)
+{
+    arr[i] = int.Parse(s[i]);
+}
+int[] brr = new int[500];
+bool flag = true;
+for(int i=0;i<arr.Length;i++)
+{
+    brr[arr[i]]++;
+}
+
+for(int i=0;i<brr.Length;i++)
+{
+    int d = brr[i] & 1;
+    if(d==1)
+    {
+        flag = false;
+        break;
+    }
+}
+
+Console.WriteLine(flag);
---
+++ src/Prolem Solving/2220. Minimum Bit Flips to Convert Number/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int n = Convert.ToInt32(Console.ReadLine());
+int p = Convert.ToInt32(Console.ReadLine());
+
+
+int count = 0;
+while (p != 0 || n!=0)
+{
+    int d = p & 1;
+    int f = n & 1;
+    Console.WriteLine(d);
+    Console.WriteLine(f);
+    if(d!=f)
+    {
+        count++;
+    }
+    n=n >> 1;
+    p = p >> 1;
+}
+Console.WriteLine(count);
