50fbef54f642b46fb5469ad7e3bc0643d6f88f8e
Sojib <mdsojibhosen444@gmail.com>
2022-10-12 21:40:51 +0600

    commit

---
+++ src/Prolem Solving/976. Largest Perimeter Triangle/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+string[] s = Console.ReadLine().Split(" ");
+
+int[] arr = new int[s.Length];
+
+for(int i=0;i<s.Length;i++)
+{
+    arr[i] = int.Parse(s[i]);
+}
+
+Array.Sort(arr);
+
+int count = 0;
+for(int i=arr.Length-3;i>=0;i--)
+{
+    if (arr[i] + arr[i + 1] > arr[i+2])
+    {
+        count=arr[i] + arr[i + 1] + arr[i + 2];
+        break;
+
+    }
+}
+
+Console.WriteLine(count);
