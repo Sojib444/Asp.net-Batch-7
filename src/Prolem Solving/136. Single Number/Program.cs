6605600d93735057001a536b763cb6f172a4efe6
Sojib <mdsojibhosen444@gmail.com>
2022-10-13 22:06:04 +0600

    commit

---
+++ src/Prolem Solving/136. Single Number/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+string[] s = Console.ReadLine().Split(" ");
+int[] arr = new int[s.Length];
+for(int i=0;i<s.Length;i++)
+{
+    arr[i] = int.Parse(s[i]);
+}
+
+int result = 0;
+int CountFrequency(int[] n)
+{
+    foreach (var item in n)
+    {
+        result ^= item;
+            
+    }
+    return result;
+}
+
+var ans = CountFrequency(arr);
+Console.WriteLine(ans);
