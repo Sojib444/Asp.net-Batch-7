53c6399277ef37af098230a177b19d68a4781f84
sojib <mdsojibhosen444@gmail.com>
2022-12-13 12:01:29 +0600

    Finally

---
+++ src/StockData.Worker/StockData.Worker/Model/StockPriceWeb.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockData.Worker.Model
+{
+    public class StockPriceWeb
+    {
+        public double LastTradingPrice { get; set; }
+        public double High { get; set; }
+        public double Low { get; set; }
+        public double ClosePrice { get; set; }
+        public double YesterdayClosePrice { get; set; }
+        public double Change { get; set; }
+        public double Trade { get; set; }
+        public double Value { get; set; }
+        public double Volume { get; set; }
+        public string CompanyId { get; set; }
+    }
+}
