85b6a88d9b460cf6971ff29d234c84e29f124e14
sojib <mdsojibhosen444@gmail.com>
2023-01-26 23:36:39 +0600

    commit

---
+++ src/Assingment-7/SQS Operation/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+using SQS_Operation;
+
+
+string? accessKey = "Sojib";
+string? secrectkey = "Aa@1312659116";
+
+public string QueueURl = "https://sqs.ap-south-1.amazonaws.com/308344596556/DevtractQueue";
+string Massage = "Hi this Sojib";
+
+Queue queue= new Queue();
+queue.AddMassage();
---
+++ src/Assingment-7/SQS Operation/Queue.cs
@@ -0,0 +1,29 @@
+﻿using Amazon.Runtime;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+namespace SQS_Operation
+{
+    public class Queue
+    {
+
+        public BasicAWSCredentials Creadential(string acccesskey, string secretKey)
+        {
+            BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(acccesskey, secretKey);
+
+            return basicAWSCredentials;
+        }
+
+        public void AddMassage(string acccesskey, string secretKey,string url,String Massage)
+        {
+            var credential = Creadential(acccesskey, secretKey);
+
+            AmazonSQSClient client = new AmazonSQSClient(credential, Amazon.RegionEndpoint.APSouth1);
+            var  massageRespons =  client.SendMessageAsync(url, Massage);
+
+            Console.WriteLine($"Massage add to the {url}");
+            Console.WriteLine($"HttpStatusCode: {massageRespons.Status}");
+
+        }
+    }
+}
