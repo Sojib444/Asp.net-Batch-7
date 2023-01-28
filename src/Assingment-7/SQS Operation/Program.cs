b0abb6bce8224be3c42b210be035c8b07f59c9a2
sojib <mdsojibhosen444@gmail.com>
2023-01-28 23:11:11 +0600

    Commit

--- src/Assingment-7/SQS Operation/Program.cs
+++ src/Assingment-7/SQS Operation/Program.cs
@@ -5,9 +5,9 @@ string? secrectkey = "";
 string queueURl = "https://sqs.ap-south-1.amazonaws.com/308344596556/SQS_QUEUE_NAME";
 string massage = $"Hi this Sojib.I send a have to send a massage in queue at {DateTime.Now}";
 
-Queue queue= new Queue();
-//await queue.AddMassage(accessKey,secrectkey, queueURl, massage);
+Queue queue = new Queue();
 
+await queue.AddMassage(accessKey,secrectkey, queueURl, massage);
 await queue.Receivemassage(accessKey, secrectkey, queueURl);
 await queue.DeleteReadMassage(accessKey, secrectkey, queueURl);
 
